bb2ca0b0e27275de44f14f8c62f161991f7c0557
revanth2595 <85432557+revanth2595@users.noreply.github.com>
2021-06-07 09:50:04 +0100

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note()  {
+    return(
+        <note>
+            <h1>Javascript and React js</h1>
+            <p>This was an amazing bootcamp taken up by Shaurya sinha. we covered everything from the scratch.
+                including Javascript React js,HTML.
+            </p>
+            </note>
+        
+    );
+}
+export default Note;
